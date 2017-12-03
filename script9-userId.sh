53210d1c649ad9398402eef2c4760a2fecd7a003
lior cohen <pppor22@gmail.com>
2017-12-03 15:42:47 +0200

    added

--- script9-userId.sh
+++ script9-userId.sh
@@ -4,4 +4,23 @@
 #date: 30/11/2017
 #homework:
 
+read -p "please enter a user name: " user #ask for user
 
+exsist=$(grep $user /etc/passwd) #check passwd for the user given
+userid=$(grep $user /etc/passwd | awk -F ":" '{print $3}') #check for userid
+
+if [[ $exsist ]]; then #if the user exsist
+
+	echo "$user exsist"
+	if [[ $userid = [5-9][0-9][0-9] || $userid = 1000 ]]; then #check if is user id is between 500-100
+
+	echo "$exsist"; #print the user data
+
+	else 
+		echo "$user - userid is'nt between 500-1000 "; #if not wait 5s and exit
+		sleep 5; exit 1;
+	fi
+else  
+		echo "$user don't exsist";  #if user don't exsist sleep 5s and exit
+		sleep 5; exit 1;
+fi
